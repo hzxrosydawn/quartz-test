77b4be8f2b65076d04e5ab620c1d0c0c1f709522
Vincent Huang <hzxrosydawn@gmail.com>
2017-11-23 23:26:57 +0800

    update some code

--- src/main/java/com/rosydawn/service/impl/JobServiceImpl.java
+++ src/main/java/com/rosydawn/service/impl/JobServiceImpl.java
@@ -9,13 +9,13 @@ import org.quartz.TriggerKey;
  * Created by Vincent on 2017-11-23.
  */
 public class JobServiceImpl implements JobService {
-    private static final Logger LOGGER = Logger.getLogger(JobServiceImpl.class);
+    private static final Logger logger = Logger.getLogger(JobServiceImpl.class);
 
     private SchedulerService schedulerService;
 
     @Override
     public void executeJob01() {
-        LOGGER.info("执行任务 开始");
+        logger.info("执行任务Job01 开始");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -25,7 +25,7 @@ public class JobServiceImpl implements JobService {
         TriggerKey triggerKey = new TriggerKey("triggerName01", "triggerGroupName01");
 
         schedulerService.updateTrigger(triggerKey);
-        LOGGER.info("执行任务 结束");
+        logger.info("执行任务Job01 结束");
     }
 
     public SchedulerService getSchedulerService() {
